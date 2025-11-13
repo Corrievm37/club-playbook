6fc36aa34acfcb157b6bddd52e1f41f1a76e7794
Corrie van Mollendorf <vprnelius.vanmollendorf@alpha.org>
2025-11-13 21:15:54 +0200

    Fix asset paths for /playbook: copy build on deploy, use SVG logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('images/logo.png') }}" alt="{{ config('app.name') }}" {{ $attributes }}>
+<img src="{{ asset('images/logo.svg') }}" alt="{{ config('app.name') }}" {{ $attributes }}>
